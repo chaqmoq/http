32f65fb0fe0f26ce24ec946bf1ef8b9b84935cbc
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-04-08 20:39:18 +0500

    Update tests

--- Tests/HTTPTests/Message/ResponseTests.swift
+++ Tests/HTTPTests/Message/ResponseTests.swift
@@ -57,139 +57,140 @@ final class ResponseTests: XCTestCase {
     func testStatuses() {
         // Assert
         for status in Response.Status.allCases {
+            let reason = status.reason
             XCTAssertEqual(status.rawValue, status.code)
 
             switch status {
             case .continue:
-                XCTAssertEqual(status.reason, "Continue")
+                XCTAssertEqual(reason, "Continue")
             case .switchProtocols:
-                XCTAssertEqual(status.reason, "Switch Protocols")
+                XCTAssertEqual(reason, "Switch Protocols")
             case .processing:
-                XCTAssertEqual(status.reason, "Processing")
+                XCTAssertEqual(reason, "Processing")
             case .earlyHints:
-                XCTAssertEqual(status.reason, "Early Hints")
+                XCTAssertEqual(reason, "Early Hints")
             case .ok:
-                XCTAssertEqual(status.reason, "OK")
+                XCTAssertEqual(reason, "OK")
             case .created:
-                XCTAssertEqual(status.reason, "Created")
+                XCTAssertEqual(reason, "Created")
             case .accepted:
-                XCTAssertEqual(status.reason, "Accepted")
+                XCTAssertEqual(reason, "Accepted")
             case .nonAuthoritativeInformation:
-                XCTAssertEqual(status.reason, "Non-Authoritative Information")
+                XCTAssertEqual(reason, "Non-Authoritative Information")
             case .noContent:
-                XCTAssertEqual(status.reason, "No Content")
+                XCTAssertEqual(reason, "No Content")
             case .resetContent:
-                XCTAssertEqual(status.reason, "Reset Content")
+                XCTAssertEqual(reason, "Reset Content")
             case .partialContent:
-                XCTAssertEqual(status.reason, "Partial Content")
+                XCTAssertEqual(reason, "Partial Content")
             case .multiStatus:
-                XCTAssertEqual(status.reason, "Multi-Status")
+                XCTAssertEqual(reason, "Multi-Status")
             case .alreadyReported:
-                XCTAssertEqual(status.reason, "Already Reported")
+                XCTAssertEqual(reason, "Already Reported")
             case .imUsed:
-                XCTAssertEqual(status.reason, "IM Used")
+                XCTAssertEqual(reason, "IM Used")
             case .multipleChoices:
-                XCTAssertEqual(status.reason, "Multiple Choices")
+                XCTAssertEqual(reason, "Multiple Choices")
             case .movedPermanently:
-                XCTAssertEqual(status.reason, "Moved Permanently")
+                XCTAssertEqual(reason, "Moved Permanently")
             case .found:
-                XCTAssertEqual(status.reason, "Found")
+                XCTAssertEqual(reason, "Found")
             case .seeOther:
-                XCTAssertEqual(status.reason, "See Other")
+                XCTAssertEqual(reason, "See Other")
             case .notModified:
-                XCTAssertEqual(status.reason, "Not Modified")
+                XCTAssertEqual(reason, "Not Modified")
             case .useProxy:
-                XCTAssertEqual(status.reason, "Use Proxy")
+                XCTAssertEqual(reason, "Use Proxy")
             case .switchProxy:
-                XCTAssertEqual(status.reason, "Switch Proxy")
+                XCTAssertEqual(reason, "Switch Proxy")
             case .temporaryRedirect:
-                XCTAssertEqual(status.reason, "Temporary Redirect")
+                XCTAssertEqual(reason, "Temporary Redirect")
             case .permanentRedirect:
-                XCTAssertEqual(status.reason, "Permanent Redirect")
+                XCTAssertEqual(reason, "Permanent Redirect")
             case .badRequest:
-                XCTAssertEqual(status.reason, "Bad Request")
+                XCTAssertEqual(reason, "Bad Request")
             case .unauthorized:
-                XCTAssertEqual(status.reason, "Unauthorized")
+                XCTAssertEqual(reason, "Unauthorized")
             case .paymentRequired:
-                XCTAssertEqual(status.reason, "Payment Required")
+                XCTAssertEqual(reason, "Payment Required")
             case .forbidden:
-                XCTAssertEqual(status.reason, "Forbidden")
+                XCTAssertEqual(reason, "Forbidden")
             case .notFound:
-                XCTAssertEqual(status.reason, "Not Found")
+                XCTAssertEqual(reason, "Not Found")
             case .methodNotAllowed:
-                XCTAssertEqual(status.reason, "Method Not Allowed")
+                XCTAssertEqual(reason, "Method Not Allowed")
             case .notAcceptable:
-                XCTAssertEqual(status.reason, "Not Acceptable")
+                XCTAssertEqual(reason, "Not Acceptable")
             case .proxyAuthenticationRequired:
-                XCTAssertEqual(status.reason, "Proxy Authentication Required")
+                XCTAssertEqual(reason, "Proxy Authentication Required")
             case .requestTimeout:
-                XCTAssertEqual(status.reason, "Request Timeout")
+                XCTAssertEqual(reason, "Request Timeout")
             case .conflict:
-                XCTAssertEqual(status.reason, "Conflict")
+                XCTAssertEqual(reason, "Conflict")
             case .gone:
-                XCTAssertEqual(status.reason, "Gone")
+                XCTAssertEqual(reason, "Gone")
             case .lengthRequired:
-                XCTAssertEqual(status.reason, "Length Required")
+                XCTAssertEqual(reason, "Length Required")
             case .preconditionFailed:
-                XCTAssertEqual(status.reason, "Precondition Failed")
+                XCTAssertEqual(reason, "Precondition Failed")
             case .payloadTooLarge:
-                XCTAssertEqual(status.reason, "Payload Too Large")
+                XCTAssertEqual(reason, "Payload Too Large")
             case .uriTooLong:
-                XCTAssertEqual(status.reason, "URI Too Long")
+                XCTAssertEqual(reason, "URI Too Long")
             case .unsupportedMediaType:
-                XCTAssertEqual(status.reason, "Unsupported Media Type")
+                XCTAssertEqual(reason, "Unsupported Media Type")
             case .rangeNotSatisfiable:
-                XCTAssertEqual(status.reason, "Range Not Satisfiable")
+                XCTAssertEqual(reason, "Range Not Satisfiable")
             case .expectationFailed:
-                XCTAssertEqual(status.reason, "Expectation Failed")
+                XCTAssertEqual(reason, "Expectation Failed")
             case .iAmATeapot:
-                XCTAssertEqual(status.reason, "I'm a teapot")
+                XCTAssertEqual(reason, "I'm a teapot")
             case .misdirectedRequest:
-                XCTAssertEqual(status.reason, "Misdirected Request")
+                XCTAssertEqual(reason, "Misdirected Request")
             case .unprocessableEntity:
-                XCTAssertEqual(status.reason,  "Unprocessable Entity")
+                XCTAssertEqual(reason,  "Unprocessable Entity")
             case .locked:
-                XCTAssertEqual(status.reason, "Locked")
+                XCTAssertEqual(reason, "Locked")
             case .failedDependency:
-                XCTAssertEqual(status.reason, "Failed Dependency")
+                XCTAssertEqual(reason, "Failed Dependency")
             case .upgradeRequired:
-                XCTAssertEqual(status.reason, "Upgrade Required")
+                XCTAssertEqual(reason, "Upgrade Required")
             case .preconditionRequired:
-                XCTAssertEqual(status.reason, "Precondition Required")
+                XCTAssertEqual(reason, "Precondition Required")
             case .tooManyRequests:
-                XCTAssertEqual(status.reason, "Too Many Requests")
+                XCTAssertEqual(reason, "Too Many Requests")
             case .requestHeaderFieldsTooLarge:
-                XCTAssertEqual(status.reason, "Request Header Fields Too Large")
+                XCTAssertEqual(reason, "Request Header Fields Too Large")
             case .connectionClosedWithoutResponse:
-                XCTAssertEqual(status.reason, "Connection Closed Without Response")
+                XCTAssertEqual(reason, "Connection Closed Without Response")
             case .unavailableForLegalReasons:
-                XCTAssertEqual(status.reason, "Unavailable For Legal Reasons")
+                XCTAssertEqual(reason, "Unavailable For Legal Reasons")
             case .clientClosedRequest:
-                XCTAssertEqual(status.reason, "Client Closed Request")
+                XCTAssertEqual(reason, "Client Closed Request")
             case .internalServerError:
-                XCTAssertEqual(status.reason, "Internal Server Error")
+                XCTAssertEqual(reason, "Internal Server Error")
             case .notImplemented:
-                XCTAssertEqual(status.reason, "Not Implemented")
+                XCTAssertEqual(reason, "Not Implemented")
             case .badGateway:
-                XCTAssertEqual(status.reason, "Bad Gateway")
+                XCTAssertEqual(reason, "Bad Gateway")
             case .serviceUnavailable:
-                XCTAssertEqual(status.reason, "Service Unavailable")
+                XCTAssertEqual(reason, "Service Unavailable")
             case .gatewayTimeout:
-                XCTAssertEqual(status.reason, "Gateway Timeout")
+                XCTAssertEqual(reason, "Gateway Timeout")
             case .httpVersionNotSupported:
-                XCTAssertEqual(status.reason, "HTTP Version Not Supported")
+                XCTAssertEqual(reason, "HTTP Version Not Supported")
             case .variantAlsoNegotiates:
-                XCTAssertEqual(status.reason, "Variant Also Negotiates")
+                XCTAssertEqual(reason, "Variant Also Negotiates")
             case .insufficientStorage:
-                XCTAssertEqual(status.reason, "Insufficient Storage")
+                XCTAssertEqual(reason, "Insufficient Storage")
             case .loopDetected:
-                XCTAssertEqual(status.reason, "Loop Detected")
+                XCTAssertEqual(reason, "Loop Detected")
             case .notExtended:
-                XCTAssertEqual(status.reason, "Not Extended")
+                XCTAssertEqual(reason, "Not Extended")
             case .networkAuthenticationRequired:
-                XCTAssertEqual(status.reason, "Network Authentication Required")
+                XCTAssertEqual(reason, "Network Authentication Required")
             case .networkConnectTimeout:
-                XCTAssertEqual(status.reason, "Network Connect Timeout")
+                XCTAssertEqual(reason, "Network Connect Timeout")
             }
         }
     }
