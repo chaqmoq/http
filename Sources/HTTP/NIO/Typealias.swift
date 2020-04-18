2e9ed52e7aa2ec3adb94b598dbd43028cdc14c74
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-04-18 18:54:32 +0500

    Add EventLoopPromise typealias

--- Sources/HTTP/NIO/Typealias.swift
+++ Sources/HTTP/NIO/Typealias.swift
@@ -1,5 +1,7 @@
 import protocol NIO.EventLoop
 import class NIO.EventLoopFuture
+import struct NIO.EventLoopPromise
 
 public typealias EventLoop = NIO.EventLoop
 public typealias EventLoopFuture = NIO.EventLoopFuture
+public typealias EventLoopPromise = NIO.EventLoopPromise
