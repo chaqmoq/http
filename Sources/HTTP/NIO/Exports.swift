5c02d33787ef2ee65066dee39cf169e193f16e6b
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-04-19 14:18:50 +0500

    Export instead of typealias

---
+++ Sources/HTTP/NIO/Exports.swift
@@ -0,0 +1,3 @@
+//@_exported import protocol NIO.EventLoop
+//@_exported import class NIO.EventLoopFuture
+//@_exported import struct NIO.EventLoopPromise
--- Sources/HTTP/NIO/Typealias.swift
+++
@@ -1,7 +0,0 @@
-import protocol NIO.EventLoop
-import class NIO.EventLoopFuture
-import struct NIO.EventLoopPromise
-
-public typealias EventLoop = NIO.EventLoop
-public typealias EventLoopFuture = NIO.EventLoopFuture
-public typealias EventLoopPromise = NIO.EventLoopPromise
