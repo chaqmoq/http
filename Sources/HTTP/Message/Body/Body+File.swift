d897661b70affd177dd96e82dc982a9eb2a26395
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-04-19 14:20:46 +0500

    Create File struct to handle file uploads

---
+++ Sources/HTTP/Message/Body/Body+File.swift
@@ -0,0 +1,13 @@
+import struct Foundation.Data
+
+extension Body {
+    public struct File {
+        public var filename: String
+        public var data: Data
+
+        public init(filename: String, data: Data) {
+            self.filename = filename
+            self.data = data
+        }
+    }
+}
