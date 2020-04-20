c253fc2fb6b604b1e74b3ad81847f5bdfc2c08a0
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-04-20 21:41:56 +0500

    Create HeaderUtil class

---
+++ Sources/HTTP/Util/HeaderUtil.swift
@@ -0,0 +1,31 @@
+import class Foundation.NSRegularExpression
+import struct Foundation.NSRange
+
+public final class HeaderUtil {
+    private init() {}
+
+    public class func getParameterValue(for key: String, in headerLine: String) -> String? {
+        let delimiter = "="
+        let pattern = ";\\s*([^=]+)=(?:\"([^\"]+)\"|([^;]+))"
+
+        if let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive) {
+            let range = NSRange(location: 0, length: headerLine.utf8.count)
+            let matches = regex.matches(in: headerLine, range: range)
+            let key = key.lowercased()
+
+            for match in matches {
+                if let range = Range(match.range, in: headerLine) {
+                    let parameter = headerLine[range].dropFirst().trimmingCharacters(in: .whitespaces)
+                    let components = parameter.components(separatedBy: delimiter)
+                    let parameterKey = components.first?.trimmingCharacters(in: .whitespaces).lowercased()
+
+                    if parameterKey == key {
+                        return components.last?.trimmingCharacters(in: .whitespaces)
+                    }
+                }
+            }
+        }
+
+        return nil
+    }
+}
