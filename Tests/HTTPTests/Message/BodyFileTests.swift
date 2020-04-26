8577911eb04dbd98afc572c49016d47c4b58b809
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-04-26 22:10:00 +0500

    Add tests to Body.File

---
+++ Tests/HTTPTests/Message/BodyFileTests.swift
@@ -0,0 +1,29 @@
+import XCTest
+@testable import HTTP
+
+final class BodyFileTests: XCTestCase {
+    func testInit() {
+        // Arrange
+        let filename = "text.txt"
+        let data = "Text".data(using: .utf8)!
+        let file = Body.File(filename: filename, data: data)
+
+        // Assert
+        XCTAssertEqual(file.filename, filename)
+        XCTAssertEqual(file.data, data)
+    }
+
+    func testUpdate() {
+        // Arrange
+        let filename = "text2.txt"
+        let data = "Another text".data(using: .utf8)!
+        var file = Body.File(filename: "text.txt", data: "Text".data(using: .utf8)!)
+
+        file.filename = filename
+        file.data = data
+
+        // Assert
+        XCTAssertEqual(file.filename, filename)
+        XCTAssertEqual(file.data, data)
+    }
+}
