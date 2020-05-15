e16923693c6fd427e9a6793df8667a4267796cf7
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-05-15 20:49:02 +0500

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -9,10 +9,10 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.15.0"),
-        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.11.0"),
-        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.4.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.1.1"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.17.0"),
+        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.12.0"),
+        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.5.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.7.2"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.1.1")
     ],
     targets: [
