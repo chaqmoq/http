4609b7ffb6205835b2fdc6ced7c880590227fb30
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-12 20:08:46 +0500

    Refactor

--- Sources/HTTP/Message/Request/Request+Description.swift
+++ Sources/HTTP/Message/Request/Request+Description.swift
@@ -1,11 +1,7 @@
 extension Request {
     public var description: String {
         var description = ""
-
-        for (name, value) in headers {
-            description.append("\(name): \(value)\n")
-        }
-
+        for (name, value) in headers { description.append("\(name): \(value)\n") }
         description.append("\n\(body)")
 
         return "\(method) \(uri) \(version)\n\(description)"
