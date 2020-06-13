8a92b8a54ae963734426f40948190906bccf4265
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-13 14:03:46 +0500

    Parse Response setCookie headers

--- Sources/HTTP/Message/Response/Response.swift
+++ Sources/HTTP/Message/Response/Response.swift
@@ -1,9 +1,12 @@
 import struct Foundation.Data
+import struct Foundation.Date
 
 public struct Response: Message {
     public var version: Version
     public var status: Status
-    public var headers: Headers
+    public var headers: Headers { didSet { setCookies() } }
+    public var cookies: Set<Cookie> { mutableCookies }
+    private var mutableCookies: Set<Cookie>
     public var body: Body { didSet { setContentLengthHeader() } }
 
     public init(
@@ -15,9 +18,11 @@ public struct Response: Message {
         self.version = version
         self.status = status
         self.headers = headers
+        mutableCookies = .init()
         self.body = body
 
         setContentLengthHeader()
+        setCookies()
     }
 
     public init(
@@ -36,5 +41,65 @@ public struct Response: Message {
         version: Version = .init()
     ) {
         self.init(.init(data: data), status: status, headers: headers, version: version)
+    }
+}
+
+extension Response {
+    mutating func setCookies() {
+        mutableCookies.removeAll()
+        let values = headers.values(for: .setCookie)
+        guard !values.isEmpty else { return }
+
+        for value in values {
+            var components = value.components(separatedBy: ";").filter { $0 != "" }
+
+            if let component = components.first {
+                components.removeFirst()
+                let subComponents = component.trimmingCharacters(in: .whitespaces).components(separatedBy: "=")
+
+                if let name = subComponents.first, let value = subComponents.last, subComponents.count == 2 {
+                    var cookie = Cookie(name: name, value: value)
+                    var cookieExists = false
+
+                    if let index = mutableCookies.firstIndex(of: cookie) {
+                        cookie = mutableCookies[index]
+                        cookieExists = true
+                    }
+
+                    for component in components {
+                        let subComponents = component.trimmingCharacters(in: .whitespaces).components(separatedBy: "=")
+
+                        if let key = subComponents.first, let optionKey = Cookie.OptionKey(rawValue: key) {
+                            let count = subComponents.count
+
+                            switch optionKey {
+                            case .expires:
+                                if let value = subComponents.last, count == 2 { cookie.expires = Date(rfc1123: value) }
+                            case .maxAge:
+                                if let value = subComponents.last, count == 2 { cookie.maxAge = Int(value) }
+                            case .domain:
+                                if let value = subComponents.last, count == 2 { cookie.domain = value }
+                            case .path:
+                                if let value = subComponents.last, count == 2 { cookie.path = value }
+                            case .isSecure:
+                                cookie.isSecure = true
+                            case .isHTTPOnly:
+                                cookie.isHTTPOnly = true
+                            case .sameSite:
+                                if let value = subComponents.last, let optionValue = Cookie.SameSite(rawValue: value) {
+                                    cookie.sameSite = optionValue
+                                }
+                            }
+                        }
+                    }
+
+                    if cookieExists {
+                        mutableCookies.update(with: cookie)
+                    } else {
+                        mutableCookies.insert(cookie)
+                    }
+                }
+            }
+        }
     }
 }
