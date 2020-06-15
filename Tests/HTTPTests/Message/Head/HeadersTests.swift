237973bd43c4ffc49cc1b53313bee14b9670b3dd
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-15 21:56:02 +0500

    Add test

--- Tests/HTTPTests/Message/Head/HeadersTests.swift
+++ Tests/HTTPTests/Message/Head/HeadersTests.swift
@@ -9,4 +9,23 @@ final class HeadersTests: XCTestCase {
         // Assert
         XCTAssertTrue(headers.isEmpty)
     }
+
+    func testInitWithDictionary() {
+        // Act
+        let headers = Headers([
+            .connection: "keep-alive",
+            .contentType: "application/json"
+        ])
+
+        // Assert
+        XCTAssertEqual(headers.count, 2)
+        XCTAssertTrue(headers.contains(where: {
+            $0.0 == HeaderName.connection.rawValue.lowercased() &&
+            $0.1 == "keep-alive"
+        }))
+        XCTAssertTrue(headers.contains(where: {
+            $0.0 == HeaderName.contentType.rawValue.lowercased() &&
+            $0.1 == "application/json"
+        }))
+    }
 }
