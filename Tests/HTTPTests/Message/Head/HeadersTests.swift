bd2691e782459857249d30abfefaf537aafb791e
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-15 21:55:43 +0500

    Add test

---
+++ Tests/HTTPTests/Message/Head/HeadersTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import HTTP
+
+final class HeadersTests: XCTestCase {
+    func testInit() {
+        // Act
+        let headers = Headers()
+
+        // Assert
+        XCTAssertTrue(headers.isEmpty)
+    }
+}
