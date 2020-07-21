f63f4d59ed5d5b8dc0fab7a34566fbeee629bfc4
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-07-21 16:02:52 +0500

    Add test

--- Tests/HTTPTests/Util/HeaderUtilTests.swift
+++ Tests/HTTPTests/Util/HeaderUtilTests.swift
@@ -27,4 +27,104 @@ final class HeaderUtilTests: XCTestCase {
         XCTAssertEqual(filenameValue, HeaderUtil.getParameterValue(named: filenameKey, in: headerLine))
         XCTAssertNil(HeaderUtil.getParameterValue(named: "invalidKey", in: headerLine))
     }
+
+    func testSetParameterValue() {
+        // Act
+        HeaderUtil.setParameterValue(nameValue, named: "", in: &headerLine)
+
+        // Assert
+        XCTAssertEqual(
+            headerLine,
+            """
+            Content-Disposition: form-data; \
+            \(nameKey!)=\"\(self.nameValue!)\"; \
+            \(filenameKey!)=\"\(self.filenameValue!)\"
+            """
+        )
+
+        // Act
+        HeaderUtil.setParameterValue(nameValue, named: "invalidKey=", in: &headerLine)
+
+        // Assert
+        XCTAssertEqual(
+            headerLine,
+            """
+            Content-Disposition: form-data; \
+            \(nameKey!)=\"\(self.nameValue!)\"; \
+            \(filenameKey!)=\"\(self.filenameValue!)\"
+            """
+        )
+
+        // Arrange
+        let nameValue = "coverPhoto"
+        let filenameValue = "coverPhoto.jpeg"
+
+        // Act
+        HeaderUtil.setParameterValue(nameValue, named: nameKey, enclosingInQuotes: true, in: &headerLine)
+
+        // Assert
+        XCTAssertEqual(
+            headerLine,
+            "Content-Disposition: form-data; \(nameKey!)=\"\(nameValue)\"; \(filenameKey!)=\"\(self.filenameValue!)\""
+        )
+
+        // Act
+        HeaderUtil.setParameterValue(filenameValue, named: filenameKey, enclosingInQuotes: true, in: &headerLine)
+
+        // Assert
+        XCTAssertEqual(
+            headerLine,
+            "Content-Disposition: form-data; \(nameKey!)=\"\(nameValue)\"; \(filenameKey!)=\"\(filenameValue)\""
+        )
+
+        // Arrange
+        let customKey = "customKey"
+        let customValue = "customValue"
+
+        // Act
+        HeaderUtil.setParameterValue(customValue, named: customKey, enclosingInQuotes: true, in: &headerLine)
+
+        // Assert
+        XCTAssertEqual(
+            headerLine,
+            """
+            Content-Disposition: form-data; \
+            \(nameKey!)=\"\(nameValue)\"; \
+            \(filenameKey!)=\"\(filenameValue)\"; \
+            \(customKey)=\"\(customValue)\"
+            """
+        )
+
+        // Arrange
+        let anotherCustomKey = "anotherCustomKey"
+        let anotherCustomValue = "anotherCustomValue"
+
+        // Act
+        headerLine += "; "
+        HeaderUtil.setParameterValue(
+            anotherCustomValue,
+            named: anotherCustomKey,
+            enclosingInQuotes: true,
+            in: &headerLine
+        )
+
+        // Assert
+        XCTAssertEqual(
+            headerLine,
+            """
+            Content-Disposition: form-data; \
+            \(nameKey!)=\"\(nameValue)\"; \
+            \(filenameKey!)=\"\(filenameValue)\"; \
+            \(customKey)=\"\(customValue)\"; \
+            \(anotherCustomKey)=\"\(anotherCustomValue)\"
+            """
+        )
+
+        // Act
+        headerLine = ""
+        HeaderUtil.setParameterValue(anotherCustomValue, named: anotherCustomKey, in: &headerLine)
+
+        // Assert
+        XCTAssertEqual(headerLine, "\(anotherCustomKey)=\(anotherCustomValue)")
+    }
 }
