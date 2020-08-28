fcb026a904e36e85cf3019a8b4668550f2de3f84
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-08-28 12:28:31 +0500

    Refactor

--- Sources/HTTP/Message/URI.swift
+++ Sources/HTTP/Message/URI.swift
@@ -3,24 +3,27 @@ import Foundation
 public struct URI {
     public static var `default`: Self { Self(string: "/")! }
 
-    public var scheme: String? { urlComponents?.scheme }
-    public var host: String? { urlComponents?.host }
-    public var port: Int? { urlComponents?.port }
-    public var url: URL? { urlComponents?.url }
-    public var string: String? { urlComponents?.string }
-    public var path: String? { urlComponents?.path }
+    public var scheme: String? { urlComponents.scheme }
+    public var host: String? { urlComponents.host }
+    public var port: Int? { urlComponents.port }
+    public var url: URL? { urlComponents.url }
+    public var string: String? { urlComponents.string }
+    public var path: String? { urlComponents.path }
     public var query: Parameters<String, String>? { getQueryParameters() }
-    private var urlComponents: URLComponents?
+    private var urlComponents: URLComponents
 
     public init?(string: String) {
-        urlComponents = URLComponents(string: string)
-        if urlComponents == nil { return nil }
+        if let urlComponents = URLComponents(string: string) {
+            self.urlComponents = urlComponents
+        } else {
+            return nil
+        }
     }
 }
 
 extension URI {
     private func getQueryParameters() -> Parameters<String, String>? {
-        if let queryItems = urlComponents?.queryItems, !queryItems.isEmpty {
+        if let queryItems = urlComponents.queryItems, !queryItems.isEmpty {
             var parameters = Parameters<String, String>()
             for queryItem in queryItems { parameters[queryItem.name] = queryItem.value }
 
