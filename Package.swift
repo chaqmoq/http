f4187dbef654534a043d70f12b8455f737a3a919
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-11-11 20:07:43 +0500

    Migrate to Swift 5.3

--- Package.swift
+++ Package.swift
@@ -8,23 +8,26 @@ let package = Package(
         .library(name: "HTTP", targets: ["HTTP"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.17.0"),
-        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.12.0"),
-        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.5.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.7.2"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.1.1")
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.23.0"),
+        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.15.0"),
+        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.7.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.10.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.2.1")
     ],
     targets: [
         .target(name: "HTTP", dependencies: [
-            "Logging",
-            "NIO",
-            "NIOHTTP1",
-            "NIOHTTP2",
-            "NIOHTTPCompression",
-            "NIOSSL"
+            .product(name: "Logging", package: "swift-log"),
+            .product(name: "NIO", package: "swift-nio"),
+            .product(name: "NIOHTTP1", package: "swift-nio"),
+            .product(name: "NIOHTTP2", package: "swift-nio-http2"),
+            .product(name: "NIOHTTPCompression", package: "swift-nio-extras"),
+            .product(name: "NIOSSL", package: "swift-nio-ssl")
         ]),
-        .testTarget(name: "HTTPTests", dependencies: ["HTTP", "AsyncHTTPClient"])
+        .testTarget(name: "HTTPTests", dependencies: [
+            .target(name: "HTTP"),
+            .product(name: "AsyncHTTPClient", package: "async-http-client")
+        ])
     ],
     swiftLanguageVersions: [.v5]
 )
