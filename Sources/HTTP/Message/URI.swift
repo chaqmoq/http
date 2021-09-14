4fd2992e4b7835b7dc3855b02a5897923cb14930
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-09-14 10:50:03 +0500

    Add `URI` documentation

--- Sources/HTTP/Message/URI.swift
+++ Sources/HTTP/Message/URI.swift
@@ -1,17 +1,37 @@
 import Foundation
 
+/// A convenience API to communicate with `URLComponents`.
 public struct URI: Encodable {
+    /// A default URI `/`.
     public static var `default`: Self { Self(string: "/")! }
 
+    /// A `scheme` subcomponent of `URLComponents`.
     public var scheme: String? { urlComponents.scheme }
+
+    /// A `host` subcomponent of `URLComponents`.
     public var host: String? { urlComponents.host }
+
+    /// A `port` subcomponent of `URLComponents`.
     public var port: Int? { urlComponents.port }
+
+    /// A `url` subcomponent of `URLComponents`.
     public var url: URL? { urlComponents.url }
+
+    /// A URL string  of `URLComponents`.
     public var string: String? { urlComponents.string }
+
+    /// A `path` subcomponent of `URLComponents`.
     public var path: String? { urlComponents.path }
+
+    /// Query parameters.
     public var query: Parameters<String, String>? { getQueryParameters() }
+
     private var urlComponents: URLComponents
 
+    /// Initializes a new instance with a URL string.
+    ///
+    /// - Warning: Returns `nil` if the string is not a valid URL string.
+    /// - Parameter string: A URL string.
     public init?(string: String) {
         if let urlComponents = URLComponents(string: string) {
             self.urlComponents = urlComponents
@@ -35,9 +55,11 @@ extension URI {
 }
 
 extension URI: Equatable {
+    /// See `Equatable`.
     public static func == (lhs: URI, rhs: URI) -> Bool { lhs.urlComponents == rhs.urlComponents }
 }
 
 extension URI: CustomStringConvertible {
+    /// See `CustomStringConvertible`.
     public var description: String { string ?? "" }
 }
