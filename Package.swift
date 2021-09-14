fbcf3dc3b723baed1c23db25ee9a5fd71d5b241a
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-09-14 10:19:51 +0500

    Remove trailing commas

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "chaqmoq-http",
     products: [
-        .library(name: "HTTP", targets: ["HTTP"]),
+        .library(name: "HTTP", targets: ["HTTP"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.15.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.7.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.10.0"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.2.1"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.2.1")
     ],
     targets: [
         .target(name: "HTTP", dependencies: [
@@ -22,12 +22,12 @@ let package = Package(
             .product(name: "NIOHTTP1", package: "swift-nio"),
             .product(name: "NIOHTTP2", package: "swift-nio-http2"),
             .product(name: "NIOHTTPCompression", package: "swift-nio-extras"),
-            .product(name: "NIOSSL", package: "swift-nio-ssl"),
+            .product(name: "NIOSSL", package: "swift-nio-ssl")
         ]),
         .testTarget(name: "HTTPTests", dependencies: [
             .target(name: "HTTP"),
-            .product(name: "AsyncHTTPClient", package: "async-http-client"),
-        ]),
+            .product(name: "AsyncHTTPClient", package: "async-http-client")
+        ])
     ],
     swiftLanguageVersions: [.v5]
 )
