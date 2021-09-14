de48d7664256f55a51f0de5668afd52bd6168ff3
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-09-14 11:11:50 +0500

    Add `Version` documentation

--- Sources/HTTP/Version.swift
+++ Sources/HTTP/Version.swift
@@ -1,7 +1,16 @@
+/// Represents an HTTP version.
 public struct Version: Encodable, Equatable {
+    /// A major HTTP version.
     public var major: Int
+
+    /// A minor HTTP version.
     public var minor: Int
 
+    /// Initializes a new instance with major and minor versions.
+    ///
+    /// - Parameters:
+    ///   - major: A major HTTP version. Defaults to `1`.
+    ///   - minor: A minor HTTP version. Defaults to `1`.
     public init(major: Int = 1, minor: Int = 1) {
         self.major = major
         self.minor = minor
@@ -9,12 +18,17 @@ public struct Version: Encodable, Equatable {
 }
 
 extension Version {
+    /// Represents a major HTTP version.
     public enum Major: Int {
+        /// The first major HTTP version.
         case one = 1
+
+        /// The second major HTTP version.
         case two
     }
 }
 
 extension Version: CustomStringConvertible {
+    /// See `CustomStringConvertible`.
     public var description: String { "HTTP/\(major).\(minor)" }
 }
