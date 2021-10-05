98b6dd86cf3d7fbcd079f187be8b06b1bb8f3795
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-10-05 16:36:27 +0500

    Update `Middleware` API

--- Sources/HTTP/Middleware.swift
+++ Sources/HTTP/Middleware.swift
@@ -1,17 +1,3 @@
 public protocol Middleware {
-    typealias RequestHandler = () -> Void
-    typealias ResponseHandler = () -> Void
-
-    func handleRequest(_ request: inout Request, nextHandler: @escaping RequestHandler) -> Any
-    func handleResponse(_ response: inout Response, nextHandler: @escaping ResponseHandler) -> Any
-}
-
-public extension Middleware {
-    func handleRequest(_ request: inout Request, nextHandler: @escaping RequestHandler) -> Any {
-        nextHandler()
-    }
-
-    func handleResponse(_ response: inout Response, nextHandler: @escaping ResponseHandler) -> Any {
-        nextHandler()
-    }
+    func handle(request: inout Request, response: inout Response, nextHandler: @escaping () -> Void)
 }
