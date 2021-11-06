1ee99d1d9ac36d7e2956859a7fd5601cab5c87dd
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-11-06 16:51:09 +0500

    Update library name and dependencies' version

--- Package.swift
+++ Package.swift
@@ -5,15 +5,15 @@ import PackageDescription
 let package = Package(
     name: "chaqmoq-http",
     products: [
-        .library(name: "ChaqmoqHTTP", targets: ["HTTP"])
+        .library(name: "HTTP", targets: ["HTTP"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
-        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.18.0"),
-        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.10.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.16.0"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.6.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0"),
         .package(name: "AnyCodable", url: "https://github.com/sukhrobkhakimov/AnyCodable.git", from: "0.6.0")
     ],
     targets: [
