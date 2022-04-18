acb59d5766d7b1c5a6de33e07f59f1bc0d28b2ac
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-18 23:12:06 +0500

    Remove unneeded exports

--- Sources/HTTP/Export.swift
+++ Sources/HTTP/Export.swift
@@ -1,6 +1,4 @@
 @_exported import class NIO.EmbeddedEventLoop
 @_exported import protocol NIO.EventLoop
-@_exported import class NIO.EventLoopFuture
 @_exported import protocol NIO.EventLoopGroup
-@_exported import struct NIO.EventLoopPromise
 @_exported import class NIO.MultiThreadedEventLoopGroup
